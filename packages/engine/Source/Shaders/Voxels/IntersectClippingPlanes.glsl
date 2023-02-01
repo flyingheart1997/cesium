ad2c74d8a13d888df93424976db2b9c3b23d9ab3
Jeshurun Hembd <jeshurun@cesium.com>
2023-02-01 09:28:16 -0500

    Begin converting voxel intersections to vec4

--- packages/engine/Source/Shaders/Voxels/IntersectClippingPlanes.glsl
+++ packages/engine/Source/Shaders/Voxels/IntersectClippingPlanes.glsl
@@ -11,7 +11,7 @@ uniform sampler2D u_clippingPlanesTexture;
 uniform mat4 u_clippingPlanesMatrix;
 
 // Plane is in Hessian Normal Form
-vec2 intersectPlane(in Ray ray, in vec4 plane) {
+vec4 intersectPlane(in Ray ray, in vec4 plane) {
     vec3 o = ray.pos;
     vec3 d = ray.dir;
     vec3 n = plane.xyz; // normal
@@ -21,50 +21,51 @@ vec2 intersectPlane(in Ray ray, in vec4 plane) {
     float b = dot(d, n);
     float t = -(w + a) / b;
 
-    if (dot(d, n) > 0.0) {
-        return vec2(t, +INF_HIT);
-    } else {
-        return vec2(-INF_HIT, t);
-    }
+    return vec4(n, t);
 }
 
 void intersectClippingPlanes(in Ray ray, inout Intersections ix) {
+    vec4 farSide = vec4(ray.dir, +INF_HIT);
+    vec4 backSide = vec4(-ray.dir, -INF_HIT);
     #if (CLIPPING_PLANES_COUNT == 1)
         // Union and intersection are the same when there's one clipping plane, and the code
         // is more simplified.
         vec4 planeUv = getClippingPlane(u_clippingPlanesTexture, 0, u_clippingPlanesMatrix);
-        vec2 intersection = intersectPlane(ray, planeUv);
-        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, intersection);
+        // The clipping volume is defined by the plane and the half-space boundary.
+        // We add these to the list in arbitrary order, since the list will be sorted anyway
+        vec4 planeIntersection = intersectPlane(ray, planeUv);
+        vec4 spaceIntersection = dot(ray.dir, planeUv.xyz) > 0.0 ? farSide : backSide;
+        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, planeIntersection, spaceIntersection);
     #elif defined(CLIPPING_PLANES_UNION)
-        float minPositiveT = +INF_HIT;
-        float maxNegativeT = -INF_HIT;
+        vec4 firstEntry = farSide;
+        vec4 lastExit = backSide;
         for (int i = 0; i < CLIPPING_PLANES_COUNT; i++) {
             vec4 planeUv = getClippingPlane(u_clippingPlanesTexture, i, u_clippingPlanesMatrix);
-            vec2 intersection = intersectPlane(ray, planeUv);
-            if (intersection.y == +INF_HIT) {
-                minPositiveT = min(minPositiveT, intersection.x);
+            vec4 intersection = intersectPlane(ray, planeUv);
+            if (dot(ray.dir, planeUv.xyz) > 0.0) {
+                firstEntry = intersection.w < firstEntry.w ? intersection : firstEntry;
             } else {
-                maxNegativeT = max(maxNegativeT, intersection.y);
+                lastExit = intersection.w > lastExit.w ? intersection : lastExit;
             }
         }
-        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX + 0, vec2(-INF_HIT, maxNegativeT));
-        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX + 1, vec2(minPositiveT, +INF_HIT));
+        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX + 0, backSide, lastExit);
+        setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX + 1, firstEntry, farSide);
     #else // intersection
-        float maxPositiveT = -INF_HIT;
-        float minNegativeT = +INF_HIT;
+        vec4 lastEntry = backSide;
+        vec4 firstExit = farSide;
         for (int i = 0; i < CLIPPING_PLANES_COUNT; i++) {
             vec4 planeUv = getClippingPlane(u_clippingPlanesTexture, i, u_clippingPlanesMatrix);
-            vec2 intersection = intersectPlane(ray, planeUv);
-            if (intersection.y == +INF_HIT) {
-                maxPositiveT = max(maxPositiveT, intersection.x);
+            vec4 intersection = intersectPlane(ray, planeUv);
+            if (dot(ray.dir, planeUv.xyz) > 0.0) {
+                lastEntry = intersection.w > lastEntry.w ? intersection : lastEntry;
             } else {
-                minNegativeT = min(minNegativeT, intersection.y);
+                firstExit = intersection.w < firstExit.w ? intersection : firstExit;
             }
         }
-        if (maxPositiveT < minNegativeT) {
-            setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, vec2(maxPositiveT, minNegativeT));
+        if (lastEntry.w < firstExit.w) {
+            setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, firstExit, lastEntry);
         } else {
-            setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, vec2(NO_HIT));
+            setIntersectionPair(ix, CLIPPING_PLANES_INTERSECTION_INDEX, vec4(NO_HIT), vec4(NO_HIT));
         }
     #endif
 }
--- packages/engine/Source/Shaders/Voxels/IntersectionUtils.glsl
+++ packages/engine/Source/Shaders/Voxels/IntersectionUtils.glsl
@@ -16,38 +16,21 @@ struct Ray {
 struct Intersections {
     // Don't access these member variables directly - call the functions instead.
 
-    #if (INTERSECTION_COUNT > 1)
-        // Store an array of intersections. Each intersection is composed of:
-        //  x for the T value
-        //  y for the shape type - which encodes positive vs negative and entering vs exiting
-        // For example:
-        //  y = 0: positive shape entry
-        //  y = 1: positive shape exit
-        //  y = 2: negative shape entry
-        //  y = 3: negative shape exit
-        // INTERSECTION_COUNT is the number of ray-*shape* intersections,
-        // so we need twice as many to track ray-*surface* intersections
-        vec2 intersections[INTERSECTION_COUNT * 2];
+    // Store an array of intersections. Each intersection is composed of:
+    //  .xyz for the surface normal at the intersection point
+    //  .w for the T value
+    // The normal is scaled by 2 for positive shapes
+    // INTERSECTION_COUNT is the number of ray-*shape* (volume) intersections,
+    // so we need twice as many to track ray-*surface* intersections
+    vec4 intersections[INTERSECTION_COUNT * 2];
 
+    #if (INTERSECTION_COUNT > 1)
         // Maintain state for future nextIntersection calls
         int index;
         int surroundCount;
         bool surroundIsPositive;
-    #else
-        // When there's only one positive shape intersection none of the extra stuff is needed.
-        float intersections[2];
     #endif
 };
-
-// Using a define instead of a real function because WebGL1 cannot access array with non-constant index.
-#if (INTERSECTION_COUNT > 1)
-    #define getIntersection(/*inout Intersections*/ ix, /*int*/ index) (ix).intersections[(index)].x
-#else
-    #define getIntersection(/*inout Intersections*/ ix, /*int*/ index) (ix).intersections[(index)]
-#endif
-
-// Using a define instead of a real function because WebGL1 cannot access array with non-constant index.
-#define getIntersectionPair(/*inout Intersections*/ ix, /*int*/ index) vec2(getIntersection((ix), (index) * 2 + 0), getIntersection((ix), (index) * 2 + 1))
 
 // Using a define instead of a real function because WebGL1 cannot access array with non-constant index.
 #if (INTERSECTION_COUNT > 1)
@@ -57,11 +40,7 @@ struct Intersections {
 #endif
 
 // Using a define instead of a real function because WebGL1 cannot access array with non-constant index.
-#if (INTERSECTION_COUNT > 1)
-    #define setIntersectionPair(/*inout Intersections*/ ix, /*int*/ index, /*vec2*/ entryExit) (ix).intersections[(index) * 2 + 0] = vec2((entryExit).x, float((index) > 0) * 2.0 + 0.0); (ix).intersections[(index) * 2 + 1] = vec2((entryExit).y, float((index) > 0) * 2.0 + 1.0)
-#else
-    #define setIntersectionPair(/*inout Intersections*/ ix, /*int*/ index, /*vec2*/ entryExit) (ix).intersections[(index) * 2 + 0] = (entryExit).x; (ix).intersections[(index) * 2 + 1] = (entryExit).y
-#endif
+#define setIntersectionPair(/*inout Intersections*/ ix, /*int*/ index, /*vec4*/ entry, /*vec4*/ exit) (ix).intersections[(index) * 2 + 0] = entry; (ix).intersections[(index) * 2 + 1] = exit
 
 #if (INTERSECTION_COUNT > 1)
 void initializeIntersections(inout Intersections ix) {
@@ -75,10 +54,10 @@ void initializeIntersections(inout Intersections ix) {
             // loop with non-constant condition, so it has to break early instead
             if (i >= n) { break; }
 
-            vec2 intersect0 = ix.intersections[i + 0];
-            vec2 intersect1 = ix.intersections[i + 1];
+            vec4 intersect0 = ix.intersections[i + 0];
+            vec4 intersect1 = ix.intersections[i + 1];
 
-            bool inOrder = intersect0.x <= intersect1.x;
+            bool inOrder = intersect0.w <= intersect1.w;
 
             ix.intersections[i + 0] = inOrder ? intersect0 : intersect1;
             ix.intersections[i + 1] = inOrder ? intersect1 : intersect0;
@@ -93,13 +72,14 @@ void initializeIntersections(inout Intersections ix) {
 #endif
 
 #if (INTERSECTION_COUNT > 1)
-vec2 nextIntersection(inout Intersections ix) {
-    vec2 entryExitT = vec2(NO_HIT);
+void nextIntersection(inout Intersections ix, in vec3 dir, out vec4 entry, out vec4 exit) {
+    entry = vec4(NO_HIT);
+    exit = vec4(NO_HIT);
 
     const int passCount = INTERSECTION_COUNT * 2;
 
     if (ix.index == passCount) {
-        return entryExitT;
+        return;
     }
 
     for (int i = 0; i < passCount; ++i) {
@@ -111,17 +91,21 @@ vec2 nextIntersection(inout Intersections ix) {
 
         ix.index = i + 1;
 
-        vec2 intersect = ix.intersections[i];
-        float t = intersect.x;
-        bool currShapeIsPositive = intersect.y < 2.0;
-        bool enter = mod(intersect.y, 2.0) == 0.0;
+        vec4 intersect = ix.intersections[i];
+        vec3 normal = intersect.xyz;
+        float t = intersect.w;
+        // Is this the main shape of the voxel primitive? Otherwise it is a
+        // negative volume (e.g., a a clipping plane or render bound)
+        bool currShapeIsPositive = dot(normal, normal) >= 2.0;
+        bool enter = dot(dir, normal) <= 0.0;
 
+        // How many nested shapes are we inside?
         ix.surroundCount += enter ? +1 : -1;
         ix.surroundIsPositive = currShapeIsPositive ? enter : ix.surroundIsPositive;
 
         // entering positive or exiting negative
         if (ix.surroundCount == 1 && ix.surroundIsPositive && enter == currShapeIsPositive) {
-            entryExitT.x = t;
+            entry = vec4(normalize(dir), t);
         }
 
         // exiting positive or entering negative after being inside positive
@@ -129,7 +113,7 @@ vec2 nextIntersection(inout Intersections ix) {
         bool exitPositive = !enter && currShapeIsPositive && ix.surroundCount == 0;
         bool enterNegativeFromPositive = enter && !currShapeIsPositive && ix.surroundCount == 2 && ix.surroundIsPositive;
         if (exitPositive || enterNegativeFromPositive) {
-            entryExitT.y = t;
+            exit = vec4(normalize(dir), t);
 
             // entry and exit have been found, so the loop can stop
             if (exitPositive) {
@@ -139,8 +123,6 @@ vec2 nextIntersection(inout Intersections ix) {
             break;
         }
     }
-
-    return entryExitT;
 }
 #endif
 
